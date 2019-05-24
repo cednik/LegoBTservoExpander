abae1688edb4384b31eabdb6e639cd6fef25b9b3
Kuba Streit <streit@robotikabrno.cz>
2019-05-24 22:03:28 +0200

    Draft of LEGO protocol parser - not tested

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,42 @@
 
 BluetoothSerial bt;
 
+#include <vector>
+#include <string>
+
+class LegoProtocol {
+public:
+    LegoProtocol()
+        :m_buffer()
+    {
+        m_buffer.reserve(2);
+    }
+
+    bool push_byte(uint8_t v) {
+        m_buffer.push_back(v);
+        return m_buffer.size() >= 2 && m_buffer.size() == packet_length();
+    }
+
+    void clear() {
+        m_buffer.clear();
+    }
+
+    uint16_t packet_length() const { return read_at<uint16_t>(0); }
+    uint16_t message_counter() const { return read_at<uint16_t>(2); }
+    uint8_t cmd_type() const { return read_at<uint8_t>(4); }
+    uint8_t cmd_number() const { return read_at<uint8_t>(5); }
+    uint8_t mailbox_length() const { return read_at<uint8_t>(6); }
+    std::string mailbox() const { return std::string(static_cast<const char*>(m_buffer.data() + 7); }
+
+private:
+    template <typename T>
+    const T& read_at (uint16_t index) const {
+        return *static_cast<T*>(m_buffer.data() + index);
+    }
+
+    std::vector<uint8_t> m_buffer;
+};
+
 void setup() {
     Serial.begin(115200);
     Serial.println("LegoBTservoExpander");
