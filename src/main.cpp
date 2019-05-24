4f6277d74da169946acc9024aa65fc6ba2425854
Kuba Streit <streit@robotikabrno.cz>
2019-05-24 19:50:48 +0200

    BT connection test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include <BluetoothSerial.h>
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial bt;
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.println("LegoBTservoExpander");
+    if (!bt.begin("Expander")) {
+        Serial.println("Can not initialize bluetooth!");
+        for(;;){}
+    }
 }
 
+bool is_connected = false;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+    bool con = bt.hasClient();
+    if (con != is_connected) {
+        is_connected = con;
+        if (is_connected) {
+            Serial.println("Client connected");
+        } else {
+            Serial.println("Client disconnected");
+        }
+    }
 }
