1575b42679c372fc4428b956e664f22e9b3ba632
Kuba Streit <kuba.streit@gmail.com>
2019-05-25 20:21:05 +0200

    Receiving test: LEGO is not able to connect to the ESP

--- src/main.cpp
+++ src/main.cpp
@@ -32,16 +32,20 @@ public:
     uint8_t cmd_type() const { return read_at<uint8_t>(4); }
     uint8_t cmd_number() const { return read_at<uint8_t>(5); }
     uint8_t mailbox_length() const { return read_at<uint8_t>(6); }
-    std::string mailbox() const { return std::string(static_cast<const char*>(m_buffer.data() + 7); }
+    std::string mailbox() const { return std::string(reinterpret_cast<const char*>(m_buffer.data() + 7)); }
+    uint16_t payload_length() const { return read_at<uint16_t>(7 + mailbox_length() + 1); }
+    const uint8_t* payload() const { return m_buffer.data() + 7 + mailbox_length() + 1 + 2; }
 
 private:
     template <typename T>
     const T& read_at (uint16_t index) const {
-        return *static_cast<T*>(m_buffer.data() + index);
+        return *reinterpret_cast<const T*>(m_buffer.data() + index);
     }
 
     std::vector<uint8_t> m_buffer;
 };
+
+LegoProtocol lego;
 
 void setup() {
     Serial.begin(115200);
@@ -54,6 +58,15 @@ void setup() {
 
 bool is_connected = false;
 
+template <class Stream, typename Value>
+void print_val(Stream& stream, std::string intro, const Value& value) {
+    stream.print(intro.c_str());
+    stream.print(": ");
+    stream.println(value);
+}
+
+#include "hexdump.h"
+
 void loop() {
     bool con = bt.hasClient();
     if (con != is_connected) {
@@ -63,5 +76,17 @@ void loop() {
         } else {
             Serial.println("Client disconnected");
         }
+    }
+    if (bt.available() && lego.push_byte(bt.read())) {
+        Serial.println("Lego packet arrived:");
+        print_val(Serial, "\tlength  ", lego.packet_length());
+        print_val(Serial, "\tmsg cnt ", lego.message_counter());
+        print_val(Serial, "\tcmd type", lego.cmd_type());
+        print_val(Serial, "\tcmd num ", lego.cmd_number());
+        print_val(Serial, "\tname len", lego.mailbox_length());
+        print_val(Serial, "\tname    ", lego.mailbox().c_str());
+        print_val(Serial, "\tdata len", lego.payload_length());
+        hexDump("\tdata:", lego.payload(), lego.payload_length());
+        lego.clear();
     }
 }
